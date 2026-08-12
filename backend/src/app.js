6a253646c6646c877a7ed47665253b785d9f945c
Nada Alahmad <nalahmad832@gmail.com.com>
2026-08-13 01:32:28 +0300

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import { errorHandler } from './middleware/error.middleware.js'
 const app = express()
 
 const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173'
-app.use(cors({ origin: clientUrl, credentials: true }))
+app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json())
 app.use(morgan('dev'))
 
